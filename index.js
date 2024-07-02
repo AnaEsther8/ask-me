b9b2513f361fe5b4931449c3463a817ee1449c62
Ana Esther <anaesther8desouza@gmail.com>
2024-07-02 11:14:35 -0300

    Implementação da página inicial

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express= require ('express')
 const app = express()
+const bodyparser = require ('body-parser')
 const port= 3000
 
 app.set('view engine' , 'ejs')
 app.use(express.static('public'))
 
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+
 app.get('/', (req,res) =>{
-    res.render("index")
+    //SELECT * FROM pergunta
+    Pergunta.findAll({raw: true , order: [
+        ['id', 'DESC']
+    ]}).then(perguntas =>{
+        res.render('index',{
+            perguntas: perguntas
+        })
+    })
 })
 
 
 app.get('/*perguntar' , (req, res ) =>{
     res.render('perguntar')
 })
+app.post('/salvarpergunta' , (req , res) =>{
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send(`Titulo : ${titulo} / Descrição: ${descricao}`)
+})
+
 app.listen(port, (erro) =>{
     if (erro){
         console.log("Erro ao iniciar o servidor")
